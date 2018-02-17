ffe89beb2327ec21e218f18bf6d873428cd35041
ariettame <36319458+ariettame@users.noreply.github.com>
2018-02-17 12:12:14 -0600

    add body padding

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import {
-  Switch,
-  Route
-} from 'react-router-dom';
-import styled from 'styled-components'
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 
-import AppNav from './components/AppNav'
-import Home from './pages/Home'
-import OurStory from './pages/OurStory'
-import Party from './pages/Party'
-import RSVP from './pages/RSVP'
-import FAQs from './pages/FAQs'
-import ContactUs from './pages/ContactUs'
-import './App.css';
+import AppNav from "./components/AppNav";
+import Home from "./pages/Home";
+import OurStory from "./pages/OurStory";
+import Party from "./pages/Party";
+import RSVP from "./pages/RSVP";
+import FAQs from "./pages/FAQs";
+import ContactUs from "./pages/ContactUs";
+import "./App.css";
 
 const Wrapper = styled.div`
-  // background-color: #BFDBD1;
-  background-color: #6AC4B8;
+  background-color: #6ac4b8;
   height: 1000px;
-`
+  padding: 17px 17px;
+`;
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
         <div className="App">
-          <AppNav></AppNav>
+          <AppNav />
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/our-story' component={OurStory} />
-            <Route exact path='/party' component={Party} />
-            <Route exact path='/RSVP' component={RSVP} />
-            <Route exact path='/faq' component={FAQs} />
-            <Route exact path='/contact' component={ContactUs} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/our-story" component={OurStory} />
+            <Route exact path="/party" component={Party} />
+            <Route exact path="/RSVP" component={RSVP} />
+            <Route exact path="/faq" component={FAQs} />
+            <Route exact path="/contact" component={ContactUs} />
           </Switch>
         </div>
       </Wrapper>
