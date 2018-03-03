7b2d713a4edeacc148582c05dcac692ad3bf20a1
Aria <36319458+ariettame@users.noreply.github.com>
2018-03-02 23:29:32 -0600

    style form elements

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -1,43 +1,68 @@
-import React from "react";
-import { Component } from "react";
+import React, { Component } from "react";
 import {
+  Grid,
+  Row,
   Col,
-  Form,
   FormGroup,
   ControlLabel,
   FormControl,
-  Button
+  DropdownButton
 } from "react-bootstrap";
+import { FormInputs } from "../components/FormInputs/FormInputs.jsx";
+import Button from "../elements/CustomButton/CustomButton.jsx";
+import Card from "../components/Card/Card.jsx";
 
 class RSVP extends Component {
   render() {
     return (
-      <div>
-        <Form horizontal>
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              Name
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col xs={12}>
+              <Card
+                title="Enter Info"
+                content={
+                  <form>
+                    <FormInputs
+                      ncols={["col-md-6", "col-md-6"]}
+                      proprieties={[
+                        {
+                          label: "Name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Name"
+                        },
+                        {
+                          label: "Email address",
+                          type: "email",
+                          bsClass: "form-control",
+                          placeholder: "Email"
+                        }
+                      ]}
+                    />
+                    <Row>
+                      <Col md={12}>
+                        <FormGroup controlId="formControlsTextarea">
+                          <ControlLabel>More Info</ControlLabel>
+                          <FormControl
+                            rows="5"
+                            componentClass="textarea"
+                            bsClass="form-control"
+                            placeholder="Here can be your description"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Button bsStyle="info" pullRight fill type="submit">
+                      Submit
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
             </Col>
-            <Col sm={10}>
-              <FormControl type="email" placeholder="Email" />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalPassword">
-            <Col componentClass={ControlLabel} sm={2}>
-              Email
-            </Col>
-            <Col sm={10}>
-              <FormControl type="password" placeholder="Password" />
-            </Col>
-          </FormGroup>
-
-          <FormGroup>
-            <Col smOffset={2} sm={10}>
-              <Button type="submit">Submit</Button>
-            </Col>
-          </FormGroup>
-        </Form>;
+          </Row>
+        </Grid>>
       </div>
     );
   }
